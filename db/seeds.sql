2a2e940c23d8e5c3e069455ff793b78ad21ff5e7
Jordan Johnson <jordaninjohnson@gmail.com>
2020-05-24 15:43:41 -0600

    Burgers can be added & Updated

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+### Seeds
+
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('jalapeno', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('bacon', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('double', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('hamburgr', false);
